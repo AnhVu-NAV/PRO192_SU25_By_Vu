855cc583f7c48f5dd47dfcfeb03c366f7313665c
Nguyen Anh Vu <vunguyen.nav2004@gmail.com>
2025-05-12 00:47:25 +0700

    b2

--- OnThucC/src/ToanTu.java
+++ OnThucC/src/ToanTu.java
@@ -13,5 +13,33 @@ public class ToanTu {
         System.out.println(10%2);
         System.out.println(5/2);
         System.out.println(5%2);
+        
+//        tiền tố: thực hiện tăng/giam trc rồi mới đến biên
+//        hậu tố: Thực hiện các taho tác với biến tồi mới  tăng/giảm
+        int a = 100;
+        System.out.println(a); //100
+        System.out.println(a++); //100 101
+        System.out.println(++a); //102
+        
+        Integer b = 10; //=> "10"
+        String s = b + "";
+        String s1 = b.toString();
+        
+        String s2= "12345";
+        
+        int c = Integer.parseInt(s2);
+        //Float.parseFloat(s)
+        
+//        if(){
+//            
+//        }        
+//        
+//        for (int i = 0; i < 10; i++) {
+//            
+//        }
+        
+        
+        
+        
     }
 }
